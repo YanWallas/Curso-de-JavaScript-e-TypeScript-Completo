466e1876a2565d44a08a02f03d067c5befa69085
Yan Wallas <yanwallas98@gmail.com>
2023-08-16 18:09:50 -0300

    Aula 008 Exercícios

---
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 008 Exercícios.js
@@ -0,0 +1,2 @@
+console.log("Meu nome é 'YAN'. Estou aprendendo JavaScript ás", 10, "da manhã.");
+console.log('Na aula de exercícios, o professor passou apenas', 1, ' exercico para diferenciar string de number!!')
