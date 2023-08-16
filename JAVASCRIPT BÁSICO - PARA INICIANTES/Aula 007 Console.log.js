f49756865658e03eccfe81e4b037d36776494c96
Yan Wallas <yanwallas98@gmail.com>
2023-08-16 18:00:49 -0300

    Aula 007 Console.log

---
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 007 Console.log.js
@@ -0,0 +1,7 @@
+console.log('Yan "Wallas"'); //String = Texto
+console.log("'Yan Wallas'");
+console.log(`Yan Wallas`);
+
+console.log(123456); //Number = Número 
+console.log(15.85); 
+console.log(35, 15.85, 'Yan Wallas de novo')
