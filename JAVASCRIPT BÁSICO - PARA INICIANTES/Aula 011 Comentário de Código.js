792df71d146e2d334a7979a4048f7febb23b51cb
Yan Wallas <yanwallas98@gmail.com>
2023-08-16 18:23:40 -0300

    Aula 011 Comentário de Código

---
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 011 Comentário de Código.js
@@ -0,0 +1,10 @@
+console.log('olá Mundo 1'); // comentario de 1 linha
+console.log('olá Mundo 2'); 
+/*console.log('olá Mundo 3'); comentario de varias linhas no JavaScript e css.!
+console.log('olá Mundo 4'); */ 
+console.log('olá Mundo 5');
+console.log('olá Mundo 6');
+console.log('olá Mundo 7');
+console.log('olá Mundo 8');
+console.log('olá Mundo 9');
+console.log('olá Mundo 10');
