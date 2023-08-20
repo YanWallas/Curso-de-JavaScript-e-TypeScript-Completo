d09018a42983b848c67a759882cf9a6718521cd5
Yan Wallas <yanwallas98@gmail.com>
2023-08-20 20:04:00 -0300

    Aula 013 Variáveis Com Let

---
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 013 Variáveis Com Let/assets/js/index.js
@@ -0,0 +1,14 @@
+let nome = 'João'; // String
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 200', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo');
+
+
+let nome1; // Declaro a variavel, mas esta sem valor
+nome1 = 'qualquer valor'; // agora a variavel foi declarada 
+console.log(nome1)
+nome1 = 'Yan'; // Declarando outro nome por cima da declaração anterior
+console.log(nome1);
