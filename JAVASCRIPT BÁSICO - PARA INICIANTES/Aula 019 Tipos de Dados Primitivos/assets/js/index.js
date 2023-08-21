1186cd2a90f93cf7a7daf726cd4db724b3879f73
Yan Wallas <yanwallas98@gmail.com>
2023-08-21 18:42:53 -0300

    Aula 019 Tipos de Dados Primitivos

--- JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 019 Tipos de Dados Primitivos/assets/js/index.js
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 019 Tipos de Dados Primitivos/assets/js/index.js
@@ -1,3 +1,6 @@
+/*tipos de dados primitivos :
+String, Number, undefined, null, Boolean e Symbol */
+
 const nome = 'Yan'; //string
 const nome1 = "Yan"; //string
 const nome2 = `Yan`; //string
@@ -8,4 +11,16 @@ let sobrenomeAluno = null; // nulo -> não aponta pra local nenhuma na memória
 const aprovado = true; // boolean = true, false (Lógico)
 
 console.log(typeof nome);
-console.log(typeof nome, nome); // traz o tipo e depois o que esta dentro da const
+console.log(typeof nome, nome); // traz o tipo e depois o que esta dentro da const.
+
+/*const a = [1, 2];
+const b = a;
+
+console.log(a, b)// valores por referencia. */
+
+let a = 2;
+const b = a;
+console.log(a, b); // 2, 2
+
+a = 3; // alterando valores da let
+console.log(a, b); // 3, 2
