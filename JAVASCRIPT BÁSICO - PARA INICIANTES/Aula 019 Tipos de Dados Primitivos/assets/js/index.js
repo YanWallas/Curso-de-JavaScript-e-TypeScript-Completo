77b3263632f772a4487926e642c304f2d8d25412
Yan Wallas <yanwallas98@gmail.com>
2023-08-21 16:58:51 -0300

    Aula 019 Tipos de Dados Primitivos

---
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 019 Tipos de Dados Primitivos/assets/js/index.js
@@ -0,0 +1,11 @@
+const nome = 'Yan'; //string
+const nome1 = "Yan"; //string
+const nome2 = `Yan`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number 
+let nomeAluno; // undefined -> não aponta pra local nenhuma na memória
+let sobrenomeAluno = null; // nulo -> não aponta pra local nenhuma na memória
+const aprovado = true; // boolean = true, false (Lógico)
+
+console.log(typeof nome);
+console.log(typeof nome, nome); // traz o tipo e depois o que esta dentro da const
