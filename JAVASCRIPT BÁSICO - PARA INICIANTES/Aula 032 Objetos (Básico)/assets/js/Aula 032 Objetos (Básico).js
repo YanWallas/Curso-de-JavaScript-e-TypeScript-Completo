00d6086d625daa756f03291b2c20ccb50ced16e2
Yan Wallas <yanwallas98@gmail.com>
2023-08-30 16:22:05 -0300

    Aula 032 Objetos (Básico)

---
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 032 Objetos (Básico)/assets/js/Aula 032 Objetos (Básico).js
@@ -0,0 +1,11 @@
+const nome01 = 'Luiz';
+const sobrenome01 = 'Miranda';
+const idade01 = 25;
+
+const nome02 = 'Maria';
+const sobrenome02 = 'Oliveira';
+const idade02 = 25;
+
+const pessoa01 = { // Criando objeto
+
+};
