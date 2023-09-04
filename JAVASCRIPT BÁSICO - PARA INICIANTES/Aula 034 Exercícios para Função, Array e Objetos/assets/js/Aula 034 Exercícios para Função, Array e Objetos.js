c530ea591bafa09f9147dc98bb20a8c874a375cb
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-04 11:01:34 -0300

    Update Aula 034 Exercícios para Função, Array e Objetos.js

--- JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 034 Exercícios para Função, Array e Objetos/assets/js/Aula 034 Exercícios para Função, Array e Objetos.js
+++ JAVASCRIPT BÁSICO - PARA INICIANTES/Aula 034 Exercícios para Função, Array e Objetos/assets/js/Aula 034 Exercícios para Função, Array e Objetos.js
@@ -1,11 +1,20 @@
 function meuEscopo() {
     const form = document.querySelector('.form');// seecionando a class form.
 
-    form.onsubmit = function (evento){// criando a função para quando aperta o button Enviar
+   /* form.onsubmit = function (evento){// criando a função para quando aperta o button Enviar
         evento.preventDefault();//previnir que a pagina seja atualizada e perda os dados do console.
         alert(1);
+        console.log('Foi enviado.');
+    };*/
+    //OUTRA FORMA DE FAZER MAIS COMUM
+    let contador = 1;
+    function recebeEventoForm(evento){// criando a function com evento criado na lista.
+        evento.preventDefault();//previnir que a pagina seja atualizada
+        console.log(`Form não for enviado ${contador}`);
+        contador++;
+    }
+    form.addEventListener('submit', recebeEventoForm);//Adicionando uma evento na lista.
 
-    };
 }
 
 meuEscopo();
