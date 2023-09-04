6019565f0d44871fdc122e34859f66d1ab5b2c01
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-04 12:30:35 -0300

    Aula 037 Operadores de Comparação

---
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 037 Operadores de Comparação/Aula 037 Operadores de Comparação.js
@@ -0,0 +1,16 @@
+/* 
+********************** OPERADORES DE COMPARAÇÃO ***************************
+> Maior que.
+>= Maior que ou igual a.
+< Menor que.
+<= Menor que ou igual a.
+= atribuição (valor recebe valor).
+== igualdade (confere valor) ****** GERALMENTE NÃO USAMOS.
+=== igualdade estrita (confere valor e tipo).
+!= diferente (confere valor) ****** GERALMENTE NÃO USAMOS.
+!== diferente estrito (confere valor e tipo).
+***************************************************************************
+*/
+const comp = 10 > 5; // vai trazer true ou false para a variavel comp.
+console.log(comp);
+
