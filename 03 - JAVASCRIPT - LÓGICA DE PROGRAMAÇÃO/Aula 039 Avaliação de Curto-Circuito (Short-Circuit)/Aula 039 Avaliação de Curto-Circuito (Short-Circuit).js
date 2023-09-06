df22f37843b2292cc33ab2cef062807bf08da92f
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-06 12:07:24 -0300

    Aula 039 Avaliação de Curto-Circuito (Short-Circuit)

---
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 039 Avaliação de Curto-Circuito (Short-Circuit)/Aula 039 Avaliação de Curto-Circuito (Short-Circuit).js
@@ -0,0 +1,13 @@
+/* 
+********************** OPERADORES LÓGICOS ***************************
+&& -> False && true -> Se false, retornara o valor que e false (parando de checar ali mesmo) -> Se true retorna o valor da ultima checagem. 
+|| -> Em inglês 'OR' -> Portugues 'ou' 
+
+False - FALSY - VALORES AVALIADO EM FALSE
+-> 0
+-> '' "" `` (Espaço facil)
+-> null / undefined
+-> NaN
+
+***************************************************************************
+*/
