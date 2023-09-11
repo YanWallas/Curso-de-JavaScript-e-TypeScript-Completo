dafd66e2ed89c4b6d3f13900eeb0d2d58c7970fc
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-11 11:37:07 -0300

    Aula 039 Avaliação de Curto-Circuito (Short-Circuit)

--- 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 039 Avaliação de Curto-Circuito (Short-Circuit)/Aula 039 Avaliação de Curto-Circuito (Short-Circuit).js
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 039 Avaliação de Curto-Circuito (Short-Circuit)/Aula 039 Avaliação de Curto-Circuito (Short-Circuit).js
@@ -1,13 +1,33 @@
 /* 
 ********************** OPERADORES LÓGICOS ***************************
 && -> False && true -> Se false, retornara o valor que e false (parando de checar ali mesmo) -> Se true retorna o valor da ultima checagem. 
-|| -> Em inglês 'OR' -> Portugues 'ou' 
+|| -> Em inglês 'OR' -> Portugues 'ou' -> Se uma das condições for true, retorna verdadeiro. 
 
-False - FALSY - VALORES AVALIADO EM FALSE
+FALSY - VALORES AVALIADO EM FALSE
+-> False
 -> 0
 -> '' "" `` (Espaço facil)
 -> null / undefined
 -> NaN
+OBS:ELEMENTOS NAO TENDO ESSAS AVALIAÇÕES, SIGNIFICA QUE SAO TRUE(VERDADEIRO)
 
 ***************************************************************************
 */
+function falaOi() {
+    return 'Oi';
+}
+
+let vaiExecutar;
+console.log(vaiExecutar && falaOi());// retorna false, não realiza a function 
+
+vaiExecutar = 'João';
+console.log(vaiExecutar && falaOi());//retorna verdadeira, então realiza a function
+
+console.log(0 || false || null || 'Luiz Otáio' || true);//retorna o primeiro elemento que encontrar verdadeiro.
+
+let corUsuario = null;
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);// retorna true(preto), porque o usuario ainda nao definiu uma cor.
+
+
