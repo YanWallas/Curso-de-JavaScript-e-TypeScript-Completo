76e7c3e5aa0affab638ed2922bfa3950071aea63
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-13 14:34:49 -0300

    Aula 040 If, else if e else pt.01

--- 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 040 If, else if e else pt.01/Aula 040 If, else if e else pt.01.js
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 040 If, else if e else pt.01/Aula 040 If, else if e else pt.01.js
@@ -3,16 +3,28 @@ Entre 12 - 17 - Boa Tarde
 Entre 18 - 23 - Boa noite
 */
 
-const hora = 20;
+// If pode ser usado sozinho.
+//Sempre que utilizo a palavra else, preciso de um if antes.
+// Eu posso ter varios else if na checagem.
+// So pode ter um else no final da checagem.
+// Podemos usar condições sem else if, utilizando apenas if e else. 
+
+const hora = 14;
 
 if (hora >= 0 && hora <= 11) {
     console.log('Bom Dia');
 } 
 
-else if (hora > 11 && hora <= 17) {
+else if (hora >= 12 && hora <= 17) {
     console.log('Boa Tarde');
 }
 
-else if (hora > 17 && hora <= 23) {
+else if (hora >= 18 && hora <= 23) {
     console.log('Boa Noite');
 }
+
+else {console.log('Olá')}
+
+const tenhoGrana = true;
+if (tenhoGrana) {console.log('Vou Sair')}
+else {console.log('Nao vou Sair')}
