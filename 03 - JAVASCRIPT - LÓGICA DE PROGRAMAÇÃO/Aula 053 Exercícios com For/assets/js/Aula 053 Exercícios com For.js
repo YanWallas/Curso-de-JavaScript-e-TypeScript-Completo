09b8dd70b214ed5fff06692be6bb1481cfb3bc42
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-22 11:02:44 -0300

    Aula 053 Exercícios com For
    Entendendo os elementos DOM com exercício.

--- 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 053 Exercícios com For/assets/js/Aula 053 Exercícios com For.js
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 053 Exercícios com For/assets/js/Aula 053 Exercícios com For.js
@@ -6,17 +6,17 @@ const elementos = [
     
 ];
 
-const container = document.querySelector('.container');
-const div = document.createElement('div');
+const container = document.querySelector('.container');//Selecionando o container.
+const div = document.createElement('div');//Criando uma tag div e jogando na variavel chamada div.
 
 for (let i = 0; i < elementos.length; i++) {
-    let { tag, texto } = elementos[i];// criando as variaveis.
-    let tagCriada = document.createElement(tag);// criando o elemento para aparecer no usuario.
-    tagCriada.innerText = texto;// colocando texto nas tag.
-    div.appendChild(tagCriada);//.appendChild Adiciona elemento no final dele.
+    let { tag, texto } = elementos[i];//Criando as variaveis e jogando dados elementos dentro da variavel.
+    let tagCriada = document.createElement(tag);// criando uma tag para com nome tag e jogando na variavel.
+    tagCriada.innerText = texto;// colocando texto na varavel.
+    div.appendChild(tagCriada);//.appendChild Adiciona um elemento filho(variavel), dentro da div.
 }
 
-container.appendChild(div);// apresentando para o usuario. 
+container.appendChild(div);// adicionando div entro de container como filho. 
 
 /* ****************************************
 ------> OUTRA FORMA DE FAZER <------
@@ -29,17 +29,17 @@ const elementos = [
     
 ];
 
-const container = document.querySelector('.container');
-const div = document.createElement('div');
+const container = document.querySelector('.container'); //Selecionando o container.
+const div = document.createElement('div'); //Criando uma tag div e jogando na variavel chamada div.
 
 for (let i = 0; i < elementos.length; i++) {
-    let { tag, texto } = elementos[i];// criando as variaveis.
-    let tagCriada = document.createElement(tag);// criando o elemento para aparecer no usuario.
-    let textoCriada = document.createTextNode(texto); //buscando texto da array e jogando na variavel para usuario.
+    let { tag, texto } = elementos[i];//Criando as variaveis e jogando dados elementos dentro da variavel.
+    let tagCriada = document.createElement(tag);//Criando uma tag para com nome tag e jogando na variavel..
+    let textoCriada = document.createTextNode(texto); //buscando texto do array e criando um nódetexto(texto para pagina) e jogando dentro da variavel.
 
-    tagCriada.appendChild(textoCriado);// colocando texto nas tag.
-    div.appendChild(tagCriada);//.appendChild Adiciona elemento no final dele.
+    tagCriada.appendChild(textoCriado);//Adicionando variavel textoCriado como filho da variavel tagCriada.
+    div.appendChild(tagCriada);//.appendChild Adiciona elemento tagCriada como filha da div.
 }
 
-container.appendChild(div);// apresentando para o usuario.
+container.appendChild(div);//Adicionando div como filho de container.
 ***************************************************** */  
