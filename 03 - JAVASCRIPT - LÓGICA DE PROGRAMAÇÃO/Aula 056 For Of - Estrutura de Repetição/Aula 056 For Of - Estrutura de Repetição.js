1eba3e31c22914d720139a5fffd6b6066946ac08
Yan Wallas <yanwallas98@gmail.com>
2023-09-25 15:49:25 -0300

    Aula 056 For Of - Estrutura de Repetição

--- 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 056 For Of - Estrutura de Repetição/Aula 056 For Of - Estrutura de Repetição.js
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 056 For Of - Estrutura de Repetição/Aula 056 For Of - Estrutura de Repetição.js
@@ -26,21 +26,39 @@ for (let valor of nome) {
 const nomes = ['Luiz', 'Otávio', 'Henrique'];
 
 for (let i = 0; i < nomes.length; i++) { // For clássico
-    console.log(nomes[i]);
+    console.log(nomes[i]);// -> Me retorna valor so indice.
 }
 
 console.log('########');
 
 for (let i in nomes) { // For in
-    console.log(i);
+    console.log(i);// -> Me retorna So o indice.
 }
 
 console.log('*********')
 
 for (let valor of nomes) { // For of
-    console.log(valor);
+    console.log(valor); // -> Me retorna so o valor.
 }
 
 nomes.forEach(function(valor, indice) { // forEach -> outra maneira de trabalhar o For trazendo valor e indice, ussando function. 
     console.log(valor, indice);
 });
+
+console.log('*********')
+
+// **** FAZENDO COM OBEJETO ****
+const pessoa = { 
+    nome: 'yan',
+    sobrenome: 'wallas'
+};
+
+for (let i in pessoa) {
+    console.log(i, pessoa[i]) // BUSCANDO NOME E O VALOR DENTRO DO NOME.
+}
+
+// OBS: Com For Of nao funciona - objeto pessoa nao e iteravel. (nao tem indice igual array).
+
+// -> For Clássico - Geralmente com iteráveis (array ou strings).
+// -> For In - Retorna o indice ou chave (strings, array ou objetos).
+// -> For Of - Retorna o valor em si (iteráveis, array ou strings).
