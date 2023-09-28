98150f7e148dc514cf703ae195b33db801089499
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-28 15:10:15 -0300

    Exerc3io 03 - Número Divisível por 3 e 5

---
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 060 á 062 Exercício com Lógica de Programação/Exercício 03 - Número Divisivel por 3 e 5.js
@@ -0,0 +1,18 @@
+/* Escreva uma função que recebe um número retorne o seguinte:
+-> Número é divisivel por 3 = fizz
+-> Número é divisivel por 5 = buzz
+-> Número é divisivel por 3 e 5 = fizzBuzz
+-> Número NÃO é divisivel por 3 e 5 = retorne o Próprio número
+-> Checar se o número é realmete um número = String
+-> Use a função com Número de 0 a 100. */
+
+//************** MINHA SOLUÇÃO ****************
+
+function resultado(){
+
+    if (isNaN(x)) {
+        return "Não e um number"
+    };
+
+    
+}
