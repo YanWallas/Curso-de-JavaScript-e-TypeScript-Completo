31ffad5c342a222d5197f7fc9c019f8a11506441
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-09-29 10:41:36 -0300

    Exercício 03 - Número Divisível por 3 e 5

--- 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 060 á 062 Exercício com Lógica de Programação/Exercício 03 - Número Divisivel por 3 e 5.js
+++ 03 - JAVASCRIPT - LÓGICA DE PROGRAMAÇÃO/Aula 060 á 062 Exercício com Lógica de Programação/Exercício 03 - Número Divisivel por 3 e 5.js
@@ -10,31 +10,25 @@
 
 function resultado(num){
     let valor = num;
-    for (let i = 0; i <= 100; i++) {
-
-        if (isNaN(valor)) {
-        return "Não e um number"
-        };
-
-        if (valor % 3 == 0 && valor % 5 == 0) {
-            return "FizzBuzz";
-        };
-
-        if (valor % 3 !== 0 && valor % 5 !== 0) {
-            return valor;
-        };
-
-        if (valor % 5 == 0) {
-            return "Buzz";
-        };
-
-        if (valor % 3 == 0) {
-            return "Fizz";
-        };
-        
-    };   
+        if (typeof n !== 'number') return "Não e um number"; // CHECAR SE O NUMERO E NUMBER MESMO.
+        if (valor % 3 === 0 && valor % 5 === 0) return "FizzBuzz";
+        if (valor % 3 !== 0 && valor % 5 !== 0) return valor;
+        if (valor % 5 === 0) return "Buzz";
+        if (valor % 3 === 0) return "Fizz"; 
+}
+for (let x = 0; x <= 100; x++) {
+console.log(x, resultado('x'));// Colocando string.
 }
 
-console.log(resultado(55));
+//************** SOLUÇÃO PROFESSOR **************
 
-//************** OUTRA SOLUÇÃO **************
+function fizzBuzz (n) {
+    if (typeof n !== 'number') return 'NaN'
+    if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
+    if (n % 3 === 0) return 'Fizz'
+    if (n % 5 === 0) return 'Buzza' 
+    return n;
+}
+for (let i = 0; i <= 100; i++) {
+    console.log(i, fizzBuzz(i)); // colocando no i para ir de 0 a 100.
+}
