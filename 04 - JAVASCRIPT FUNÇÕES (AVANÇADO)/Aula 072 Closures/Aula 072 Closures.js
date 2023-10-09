7a4f1d66347498a3d86e39e446f51a7d6449e660
Yan Wallas <yanwallas98@gmail.com>
2023-10-09 16:11:59 -0300

    Aula 072 Closures

--- 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 072 Closures/Aula 072 Closures.js
+++ 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 072 Closures/Aula 072 Closures.js
@@ -7,3 +7,16 @@ function retornFuncao() {
 
 const funcao = retornFuncao();
 console.log(funcao);
+console.dir(funcao);// mesmo coisa que console.log.
+
+
+function retornFuncao1(nome) {
+    return function () {
+        return nome;
+    };
+}
+
+const funcao1= retornFuncao1('Luiz');
+const funcao2= retornFuncao1('Jõao');
+console.dir(funcao1());
+console.dir(funcao2());
