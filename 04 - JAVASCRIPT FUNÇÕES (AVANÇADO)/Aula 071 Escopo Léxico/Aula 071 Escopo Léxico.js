edb8b44898fdb309635c3539037787b3eebce2ee
Yan Wallas <yanwallas98@gmail.com>
2023-10-09 16:50:00 -0300

    Aula 071 Escopo Léxico

--- 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 071 Escopo Léxico/Aula 071 Escopo Léxico.js
+++ 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 071 Escopo Léxico/Aula 071 Escopo Léxico.js
@@ -5,6 +5,8 @@ function falaNome() {
     console.log(nome); // posso usar a const declarada de fora da function.
 }
 
+/**********************************************/
+
 function usaFalaNome() {
     const nome = 'Otavio'; // ela nao vai le essa variavel, pq ta chamando a function falaNome() e dentro dela, ela busca a variavel de fora.
     falaNome(); // puxanso function dentro da uma function.
