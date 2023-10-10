19a299c19d154f1da2d633d6bf658d1a728ba4ff
Yan Wallas <yanwallas98@gmail.com>
2023-10-10 16:39:48 -0300

    Aula 075 Funções Fábrica (Factory Functions)

--- 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 075 Funções Fábrica (Factory Functions)/Aula 075 Funções Fábrica (Factory Functions).js
+++ 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 075 Funções Fábrica (Factory Functions)/Aula 075 Funções Fábrica (Factory Functions).js
@@ -1,4 +1,5 @@
-// function que restun objetos.
+// FACTORY FUNCTION (FUNÇÃO FÁBRICA)
+//FUNCTION QUE RETURN OBJETOS.
 
 // this -> Sempre vai se referir o objeto que chamou a função
 // this no cado de baixo e a const p1, pq ele esta chamando a função, ou seja,
@@ -23,7 +24,74 @@ function criaPessoa(nome, sobrenome, altura, peso){
 
 const p1 = criaPessoa('Luiz', 'Otavio', 1.8, 80);
 const p2 = criaPessoa('Maria', 'joaquina', 1.6, 50); // Agora p2 que esta chamando o this.nome.
-console.log(p1.fala('falando sore js'));//Exibindo p1 e chamando a function fala que esta dentro dela
+console.log(p1.fala('falando sobre js'));//Exibindo p1 e chamando a function fala que esta dentro dela
 console.log(`O imc e de ${p1.imc()}`);// exibindo o imc. a function dentro do p1.
 console.log(p2.fala('chamando o this agora'));
 console.log(`O imc e de ${p2.imc()}`);
+
+/**********************************************/
+
+// CONSTRUCTOR FUNCTION (FUNÇÃO CONSTRUTORA)
+
+function criaPessoa1(nome, sobrenome, altura, peso){ 
+    return {
+        nome,
+        sobrenome,
+        fala(assunto){ // criando uma function.
+            return `${this.nome} está ${assunto}.`;
+        },
+        peso,
+        altura,
+        // Getter -> para obter apenas o valor que a função return.
+        get imc() {// Colocando get antes da função, ele se comporta como atibuto.
+            const indice = this.peso / (this.altura **2);
+            return indice.toFixed(2);
+        }
+    };
+}
+
+const p4 = criaPessoa1('YAN', 'WALLAS', 1.8, 80);
+console.log(p4.nome);
+console.log(p4.sobrenome);
+console.log(p4.imc);//Puxando como atributo e nao igual a função(p4.imc()).
+
+/**********************************************/
+
+function criaPessoa2(nome, sobrenome, altura, peso){ 
+    return {
+        nome,
+        sobrenome,
+
+        // Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+            //return `${nome} ${sobrenome}` -> sem 'this' nao altera o nome.
+        },
+
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');//vai retorna o nome mudado depois, em array (separando o nome atraves do espaço)
+            this.nome = valor.shift();//Vai jogar o primeiro valor do array na variavel nome de fora(por isso 'this' antes), e tirar ele.
+            this.sobrenome = valor.join(' ');// jogando o resto do array em sobrenome com o espaço estre os nomes.
+        },
+
+        fala(assunto){ // criando uma function.
+            return `${this.nome} está ${assunto}.`;
+        },
+
+        peso,
+        altura,
+
+        // Getter -> para obter apenas o valor que a função return.
+        get imc() {// Colocando get antes da função, ele se comporta como atibuto.
+            const indice = this.peso / (this.altura **2);
+            return indice.toFixed(2);
+        }
+    };
+}
+
+const p5 = criaPessoa2('elaine', 'cristina', 1.8, 80);
+p5.nomeCompleto = 'Elaine Cristina Almeida'; // mudando o conteudo da função depois.
+console.log(p5.nome);
+console.log(p5.sobrenome);
+console.log(p5.nomeCompleto);
