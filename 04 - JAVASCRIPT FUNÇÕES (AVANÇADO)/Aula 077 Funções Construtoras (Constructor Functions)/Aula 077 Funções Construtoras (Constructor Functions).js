f57a295116ab3cdb0999b408d25be5da49ea40b7
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-10-16 14:17:42 -0300

    Aula 077 Funções Construtoras (Constructor Functions)

--- 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 077 Funções Construtoras (Constructor Functions)/Aula 077 Funções Construtoras (Constructor Functions).js
+++ 04 - JAVASCRIPT FUNÇÕES (AVANÇADO)/Aula 077 Funções Construtoras (Constructor Functions)/Aula 077 Funções Construtoras (Constructor Functions).js
@@ -3,9 +3,22 @@
 // Construtora -> Pessoa (new) -> new e Obrigatorio para chamar a function.
 
 function Pessoa(nome,sobrenome) {
+    const ID = 123456; //variavel privadas
+
+    const metodoInterno = () => {
+
+    };
+
+    // Atributos ou métodos públicos (this -> anotação de ponto)
     this.nome = nome;
     this.sobrenome = sobrenome;
+
+    this.metodo = () => {
+        console.log(this.nome + ': Sou um método')
+    };
 }
 
 const p1 = new Pessoa('Luiz', 'Otávio');
 const p2 = new Pessoa('Maria', 'Oliveira')
+p1.metodo();
+p2.metodo();
