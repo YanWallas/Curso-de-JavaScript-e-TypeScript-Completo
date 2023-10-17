9bf77b6480fddcc9479b25a48de095bb48197207
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-10-17 14:30:15 -0300

    Aula 084 Filter - Filtrando o Array

---
+++ 05 - JAVASCRIPT ARRAYS (AVANÇADO)/Aula 084 Filter - Filtrando o Array/Aula 084 Filter - Filtrando o Array.js
@@ -0,0 +1,22 @@
+// Filter, Map, Reduce
+
+// retorne os números maiores que 10
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+
+
+
+/**********************************************/
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas om mais de 50 anos 
+// Retorne as pessoas cujo nome termina com 'a'
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Leticia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallas', idade: 47},
+]
