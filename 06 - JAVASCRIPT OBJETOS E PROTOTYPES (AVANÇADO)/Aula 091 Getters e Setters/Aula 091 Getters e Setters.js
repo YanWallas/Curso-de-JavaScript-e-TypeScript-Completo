a9b18b02ee4039ce8025fcb9062a7294b206a45d
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-10-23 12:11:22 -0300

    Aula 091 Getters e Setters

--- 06 - JAVASCRIPT OBJETOS E PROTOTYPES (AVANÇADO)/Aula 091 Getters e Setters/Aula 091 Getters e Setters.js
+++ 06 - JAVASCRIPT OBJETOS E PROTOTYPES (AVANÇADO)/Aula 091 Getters e Setters/Aula 091 Getters e Setters.js
@@ -2,6 +2,8 @@
 
 // set -> Permite que você armazene valores exclusivos de qualquer tipo, desde valores primitivos a referências a objetos.
 
+//OBS: Usando get/set -> nao usa value e writable.
+
 function Produto(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
@@ -13,9 +15,13 @@ function Produto(nome, preco, estoque) {
         enumerable: true, // True mostra a chave. false nao mostra a chave.
         configurable: true, // Pode apagar/editar/reconfigurar a chave.(true/false)
         get: function() { 
-            return estoque;
+            return estoquePriv;
         },
         set: function(valor) {
+            if (typeof valor !== 'number') {
+                console.log('Diferente de Number');
+                throw new TypeError('Digite um Number para modificacar o estoque');//Jogando um erro no terminal.
+            }
             estoquePriv =  valor;
         }
     });
@@ -23,5 +29,25 @@ function Produto(nome, preco, estoque) {
 
 const p1 = new Produto('Camiseta',20, 3);
 console.log(p1);
-p1.estoque = 'Ovalor que eu quero';
+p1.estoque = 500;// se eu jogando String aparece o erro do set.
 console.log(p1.estoque);
+
+/*********************************************************************/
+
+function criaProduto(nome) {
+    return {
+        get nome() {
+            return nome;
+        },
+        set nome(valor) {
+            valor = valor.replace('preta', '');//retiran a palavra preta por espaço vazio.
+            nome = valor;
+        }
+    };
+}
+const p2 = criaProduto('Camiseta');
+console.log(p2);
+console.log(p2.nome);
+
+p2.nome = 'blusa preta';
+console.log(p2.nome);
