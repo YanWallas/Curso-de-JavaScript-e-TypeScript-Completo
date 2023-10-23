a517524407a9a27a770fd08ce30f7ffbe3d15bbc
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-10-23 12:44:49 -0300

    Aula 092 Métodos úteis para Objetos

---
+++ 06 - JAVASCRIPT OBJETOS E PROTOTYPES (AVANÇADO)/Aula 092 Métodos úteis para Objetos/Aula 092 Métodos úteis para Objetos.js
@@ -0,0 +1,31 @@
+/*
+Object.values
+Object.entries
+Object.assign(des, any)
+Object.getOwnPropertyDescriptor(o, 'prop')
+... (spread)
+
+// Já vimos 
+Object.keys (retirna as chaves)
+Object.freeze (Congela o objeto)
+Object.defineProperty (define uma propriedade)
+*/
+
+//Obejto por referencia(muda um, muda outro)
+const produto = {nome: 'Caneca', preco: 1.8};
+const outraCoisa = produto;
+
+outraCoisa.nome = 'Caneca alterada';
+console.log(produto);
+console.log(outraCoisa);
+
+//Obejto por Copia(muda um, NÂO muda outro) Muda apenas se for o produto1(Original)
+const produto1 = {nome: 'Camiseta', preco: 1.8};
+const outraCoisa1 = {
+    ...produto1, //Espalhando a variavel dentro da outra
+    materia: 'lã' // Adicionando outro elemento.
+};
+
+outraCoisa1.nome = 'Camiseta alterada';
+console.log(produto1);
+console.log(outraCoisa1);
