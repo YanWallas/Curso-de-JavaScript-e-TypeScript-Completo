5510e6344362c959d153526910d72301d2e15308
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-10-23 10:13:28 -0300

    Aula 091 Getters e Setters

--- 06 - JAVASCRIPT OBJETOS E PROTOTYPES (AVANÇADO)/Aula 091 Getters e Setters/Aula 091 Getters e Setters.js
+++ 06 - JAVASCRIPT OBJETOS E PROTOTYPES (AVANÇADO)/Aula 091 Getters e Setters/Aula 091 Getters e Setters.js
@@ -1,23 +1,27 @@
+// get -> Associa uma propriedade de um objeto a uma função que será chamada quando tal propriedade é acessada.
+
+// set -> Permite que você armazene valores exclusivos de qualquer tipo, desde valores primitivos a referências a objetos.
 
 function Produto(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
 
+    let estoquePriv = estoque;
+
     Object.defineProperty(this, 'estoque', { // Mexendo e apenas uma propriedade do objeto.
 
         enumerable: true, // True mostra a chave. false nao mostra a chave.
-        value: estoque, // Valor da chave
-        writable: false,// Se pode alterar o valor da chave.(true/false)
-        configurable: true // Pode apagar/editar/reconfigurar a chave.(true/false)
-
+        configurable: true, // Pode apagar/editar/reconfigurar a chave.(true/false)
+        get: function() { 
+            return estoque;
+        },
+        set: function(valor) {
+            estoquePriv =  valor;
+        }
     });
-
 }
 
 const p1 = new Produto('Camiseta',20, 3);
-console.log(Object.keys(p1)); 
 console.log(p1);
-
-for (let chave in p1) {
-    console.log(chave);
-}
+p1.estoque = 'Ovalor que eu quero';
+console.log(p1.estoque);
