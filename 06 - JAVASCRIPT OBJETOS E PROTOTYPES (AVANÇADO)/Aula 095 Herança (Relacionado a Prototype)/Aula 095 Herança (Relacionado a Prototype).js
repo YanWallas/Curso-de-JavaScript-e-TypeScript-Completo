a61034126a4d0bc5b57e78b559435bcac866e536
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-10-26 11:08:08 -0300

    Aula 095 Herança (Relacionado a Prototype)

--- 06 - JAVASCRIPT OBJETOS E PROTOTYPES (AVANÇADO)/Aula 095 Herança (Relacionado a Prototype)/Aula 095 Herança (Relacionado a Prototype).js
+++ 06 - JAVASCRIPT OBJETOS E PROTOTYPES (AVANÇADO)/Aula 095 Herança (Relacionado a Prototype)/Aula 095 Herança (Relacionado a Prototype).js
@@ -14,12 +14,38 @@ Produto.prototype.desconto = function(quantia){
 
 function Camiseta(nome, preco, cor){
     Produto.call(this,nome,preco);
+    this.cor = cor;
 }
 Camiseta.prototype = Object.create(Produto.prototype);
 Camiseta.prototype.constructor = Camiseta;
 
+Camiseta.prototype.aumento = function(percentual){//Sobreescrevendo o aumento para porcentagem.
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function () {
+            return estoque;
+        },
+        set: function (valor) {
+            if (typeof valor !== 'number') return;
+            estoque = valor;
+        }
+    })
+}
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
 const produto = new Produto('Gen', 111);
 const camiseta = new Camiseta('Regata', 7.5, 'Preta');
+const caneca = new Caneca('Caneca', 15, 'Porcelana', 8);
 
 console.log(produto);
 console.log(camiseta);
+console.log(caneca);
