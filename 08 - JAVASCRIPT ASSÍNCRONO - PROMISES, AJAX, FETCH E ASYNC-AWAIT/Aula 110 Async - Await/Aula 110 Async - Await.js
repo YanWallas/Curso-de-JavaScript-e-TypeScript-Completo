aeb325b6397d4af9270dcbeafb8611ccd73653e2
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-11-08 11:08:26 -0300

    Aula 110 Async - Await

---
+++ 08 - JAVASCRIPT ASSÍNCRONO - PROMISES, AJAX, FETCH E ASYNC-AWAIT/Aula 110 Async - Await/Aula 110 Async - Await.js
@@ -0,0 +1,19 @@
+function rand(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof msg !== 'string') {
+                reject('CAI NO ERRO');
+                return;
+            }
+
+            resolve(msg.toUpperCase() + ' - Passei na promise');
+            return;
+        }, tempo);
+    });
+}
