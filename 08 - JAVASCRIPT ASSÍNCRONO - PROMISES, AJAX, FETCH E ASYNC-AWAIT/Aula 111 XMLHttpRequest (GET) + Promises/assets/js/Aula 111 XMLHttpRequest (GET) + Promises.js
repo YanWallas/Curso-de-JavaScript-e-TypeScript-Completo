bf751be310ac804f3fde7e05cac0162e7f77b8b3
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-11-10 11:07:09 -0300

    Aula 111 XMLHttpRequest (GET) + Promises

--- 08 - JAVASCRIPT ASSÍNCRONO - PROMISES, AJAX, FETCH E ASYNC-AWAIT/Aula 111 XMLHttpRequest (GET) + Promises/assets/js/Aula 111 XMLHttpRequest (GET) + Promises.js
+++ 08 - JAVASCRIPT ASSÍNCRONO - PROMISES, AJAX, FETCH E ASYNC-AWAIT/Aula 111 XMLHttpRequest (GET) + Promises/assets/js/Aula 111 XMLHttpRequest (GET) + Promises.js
@@ -4,6 +4,7 @@ um URL sem precisar atualizar completamente a página. Isso permite que uma pág
 parte de uma página sem interromper o que o usuário está fazendo.
 
 XMLHttpRequesté muito usado na programação AJAX .*/
+/* FORMA ANTIGA DE RESOLVER ESSA.
 
 const request = obj => {
     const xhr = new XMLHttpRequest();
@@ -11,6 +12,93 @@ const request = obj => {
     xhr.send(null);
 
     xhr.addEventListener('load', () => {
-        
-    })
+        if(xhr.status >= 20 && xhr.status <300) {
+            obj.success(xhr.responseText);
+        } else {
+            obj.error(xhr.statusText);
+        }
+    });
 };
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
+    const objConfig = {
+        method: 'GET',
+        url: href,
+        success(response) {
+            carregaResultado(response);
+        },
+        error(errorText) {
+            console.log(errorText);
+        }
+    }
+
+    request(objConfig);
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}*/
+
+// RESOLVENDO COM AJAX + PROMISSE AGORA
+
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send(null);
+
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 20 && xhr.status <300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        }); 
+    });
+};
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
+    const objConfig = {
+        method: 'GET',
+        url: href,
+    }
+
+    try {
+        const response = await request(objConfig);
+        carregaResultado(response);
+    } catch (e) {
+        console.log(e);
+    }
+    
+   
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
