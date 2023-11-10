c409f3303345df42ddad836a2a22aca226bc4318
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-11-10 12:36:36 -0300

    Aula 112 Fetch API (GET)

---
+++ 08 - JAVASCRIPT ASSÍNCRONO - PROMISES, AJAX, FETCH E ASYNC-AWAIT/Aula 112 Fetch API (GET)/assets/js/Aula 112 Fetch API (GET).js
@@ -0,0 +1,80 @@
+// COM AJAX + PROMISSE AGORA
+/*
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send(null);
+
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status <300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        }); 
+    });
+};
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
+    const objConfig = {
+        method: 'GET',
+        url: href,
+    }
+
+    try {
+        const response = await request(objConfig);
+        carregaResultado(response);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}*/
+
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send(null);
+
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status <300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        }); 
+    });
+};
+
+async function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
+    try {
+        const response = await request(objConfig);
+        carregaResultado(response);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
