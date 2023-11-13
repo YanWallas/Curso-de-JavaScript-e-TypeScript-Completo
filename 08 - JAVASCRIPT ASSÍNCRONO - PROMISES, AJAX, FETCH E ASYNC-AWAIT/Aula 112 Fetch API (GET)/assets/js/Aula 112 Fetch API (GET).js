daebbbb100ad881620b31ebeda8701b6ae071e0d
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-11-13 09:58:24 -0300

    Aula 112 Fetch API (GET)

--- 08 - JAVASCRIPT ASSÍNCRONO - PROMISES, AJAX, FETCH E ASYNC-AWAIT/Aula 112 Fetch API (GET)/assets/js/Aula 112 Fetch API (GET).js
+++ 08 - JAVASCRIPT ASSÍNCRONO - PROMISES, AJAX, FETCH E ASYNC-AWAIT/Aula 112 Fetch API (GET)/assets/js/Aula 112 Fetch API (GET).js
@@ -47,28 +47,43 @@ function carregaResultado(response) {
     resultado.innerHTML = response;
 }*/
 
-const request = obj => {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(obj.method, obj.url, true);
-        xhr.send(null);
+/****************************************************************/
 
-        xhr.addEventListener('load', () => {
-            if(xhr.status >= 200 && xhr.status <300) {
-                resolve(xhr.responseText);
-            } else {
-                reject(xhr.statusText);
-            }
-        }); 
-    });
-};
+// METODOS MAIS CURTOS DE ESCREVER A FUNCTION COM EVENTO PROMISSE.
 
-async function carregaPagina(el) {
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+/* -> MANEIRA REDUZIDA DE FAZER ESSA FUNCTION COM PROMISSE
+
+function carregaPagina(el) {
     const href = el.getAttribute('href');
 
+    fetch(href)
+        .then(response => {
+            if(response.status !== 200) throw new Error('ERRO 404!');
+            return response.text()
+        })
+        .then(html => carregaResultado(html))
+        .catch(e => console.log(e));
+}*/
+
+async function carregaPagina(el) {//-> MANEIRA CURTA COM ASYNC DA FUNÇÃO COM PROMISSE.
     try {
-        const response = await request(objConfig);
-        carregaResultado(response);
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+
+        if(response.status !== 200) throw new Error('ERRO 404!');
+
+        const html = await response.text();
+        carregaResultado(html);
     } catch (e) {
         console.log(e);
     }
