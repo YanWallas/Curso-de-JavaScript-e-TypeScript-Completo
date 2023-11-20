2f999836b39c1a9cc555c288f57c253f30c57e70
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2023-11-20 10:50:52 -0300

    Aula 118 ES6 Modules Import-Export

---
+++ 09 - JAVASCRIPT TOOLING E ES6 MODULES - MÓDULOS/Aula 118 ES6 Modules Import-Export/public/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//1
+//2
+//3
+alert('Olá Mundo!');
+console.log('Olá Mundo');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ 09 - JAVASCRIPT TOOLING E ES6 MODULES - MÓDULOS/Aula 118 ES6 Modules Import-Export/src/index.js
@@ -0,0 +1,5 @@
+//1
+//2
+//3
+alert('Olá Mundo!')
+console.log('Olá Mundo')
