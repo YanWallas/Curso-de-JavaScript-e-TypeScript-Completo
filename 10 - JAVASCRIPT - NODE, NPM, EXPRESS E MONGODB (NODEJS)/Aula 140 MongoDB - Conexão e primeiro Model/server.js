f6c930e146294798cd7ad49e8bcfc36ef1032712
Yan Wallas <yanwallas98@gmail.com>
2023-12-16 14:53:12 -0300

    conexão com servidor

--- 10 - JAVASCRIPT - NODE, NPM, EXPRESS E MONGODB (NODEJS)/Aula 140 MongoDB - Conexão e primeiro Model/server.js
+++ 10 - JAVASCRIPT - NODE, NPM, EXPRESS E MONGODB (NODEJS)/Aula 140 MongoDB - Conexão e primeiro Model/server.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');//puxando banco de dados
+
+const connectionsString = 'mongodb+srv://yanwallas:yan0108wallas@cursojs.hmzx7zd.mongodb.net/?retryWrites=true&w=majority';
+
+//mongoose.connect(connectionsString, { useNewUrlParser: true, useUnifiedTopology: true } -> Caso apareca mensagem no banco de dados, utilizar o que esta dentro de {}.
+
+mongoose.connect(connectionsString) //caso de mensagem no banco de dados, coloque { useNewUrlParser: true, useUnifiedTopology: true }.
+    .then(() => {
+        console.log('Conectei á base de dados.');
+        app.emit('Pronto');//Para emitir o sinal do Bando de dados conectado.
+    })
+    .catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
 const meuMiddleware = require('./src/middlewares/middleware');
@@ -16,7 +31,10 @@ app.set('view engine', 'ejs');
 app.use(meuMiddleware);//vai passar por todos os middlewares.
 app.use(routes);//Usa minhas rotas.
 
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');//3000 e o número da porta do servior.
-    console.log('Servidor executando na porta 300');
-});// selecionando a porta do servidor.
+app.on('Pronto', () => { //So vai conectar no servidor, depois que estiver pronto. 
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');//3000 e o número da porta do servior.
+        console.log('Servidor executando na porta 300');
+    });// selecionando a porta do servidor.
+});
+
