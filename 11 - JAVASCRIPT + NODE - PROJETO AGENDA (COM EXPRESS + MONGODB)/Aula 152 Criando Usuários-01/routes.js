0d7b489c5fd33cf628e794fc361101206cdd8ea2
Yan Wallas <yanwallas98@gmail.com>
2024-01-10 15:46:56 -0300

    Aula 152 Criando Usuários-01

---
+++ 11 - JAVASCRIPT + NODE - PROJETO AGENDA (COM EXPRESS + MONGODB)/Aula 152 Criando Usuários-01/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');//importando um controller.
+const loginController = require('./src/controllers/loginController');
+
+
+// Route -> VAI TRATAR DAS ROTAS
+
+// Rotas da home
+route.get('/', homeController.index);
+
+// Rotas de login
+route.get('/login/index', loginController.index);
+
+
+
+module.exports = route;
