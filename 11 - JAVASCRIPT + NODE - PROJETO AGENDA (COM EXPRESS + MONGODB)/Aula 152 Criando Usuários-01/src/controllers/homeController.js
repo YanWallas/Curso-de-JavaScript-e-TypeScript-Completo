385f29d657317ca773bde43f03d33e2d760b02f5
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2024-01-11 11:08:31 -0300

    Aula 152 Criando Usuários-01

--- 11 - JAVASCRIPT + NODE - PROJETO AGENDA (COM EXPRESS + MONGODB)/Aula 152 Criando Usuários-01/src/controllers/homeController.js
+++ 11 - JAVASCRIPT + NODE - PROJETO AGENDA (COM EXPRESS + MONGODB)/Aula 152 Criando Usuários-01/src/controllers/homeController.js
@@ -1,6 +1,6 @@
 //FUNCTION DE TRATAMENTO.
 
 exports.index = (req, res) => {//Caminho
-    res.render('index');// vai renderizar o index dentro da pasta views.
+  res.render('index');// vai renderizar o index dentro da pasta views.
 
 };
--- 11 - JAVASCRIPT + NODE - PROJETO AGENDA (COM EXPRESS + MONGODB)/Aula 152 Criando Usuários-01/src/controllers/loginController.js
+++ 11 - JAVASCRIPT + NODE - PROJETO AGENDA (COM EXPRESS + MONGODB)/Aula 152 Criando Usuários-01/src/controllers/loginController.js
@@ -1,26 +1,26 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+  res.render('login');
 }
 
 exports.register = async function (req, res) {
-    try {
-        const login = new Login(req.body);
-        await login.register(); // Chamando a função do login model.
+  try {
+    const login = new Login(req.body);
+    await login.register(); // Chamando a função do login model.
 
-        if(login.errors.length > 0) {
-            req.flash('errors', login.errors);
-            req.session.save(function() {
-                return res.redirect('back');
-            });
-            return;
-        }
-
-        return res.send(login.errors);
-
-    } catch (e) {
-        console.log(e);
-        return res.render('404');
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+      return;
     }
+
+    return res.send(login.errors);
+
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
+  }
 };
