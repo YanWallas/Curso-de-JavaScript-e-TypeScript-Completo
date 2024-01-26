7a5032721f62186fc23aaf7ccbb8bc64128db83c
Yan Wallas <yanwallas98@gmail.com>
2024-01-26 17:04:49 -0300

    Aula 157 Listando e Apagando Contatos (Read e Delete)

--- 11 - JAVASCRIPT + NODE - PROJETO AGENDA (COM EXPRESS + MONGODB)/Aula 157 Listando e Apagando Contatos (Read e Delete)/src/models/contatoModel.js
+++ 11 - JAVASCRIPT + NODE - PROJETO AGENDA (COM EXPRESS + MONGODB)/Aula 157 Listando e Apagando Contatos (Read e Delete)/src/models/contatoModel.js
@@ -79,7 +79,7 @@ Contato.buscaContatos = async function() {
 
 Contato.delete = async function(id) {
   if(typeof id !== 'string') return;
-  const contato = await ContatoModel.findOneAndDelete(id);
+  const contato = await ContatoModel.findOneAndDelete({_id: id});
   return contato;
 };
 
