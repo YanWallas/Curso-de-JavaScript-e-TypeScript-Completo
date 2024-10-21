939f1e343a65b13be0b7e5318cac22d08746fa13
Yan Wallas <122409686+YanWallas@users.noreply.github.com>
2024-10-21 12:50:57 -0300

    Limpando desnecessários

--- 10 - JAVASCRIPT - NODE, NPM, EXPRESS E MONGODB (NODEJS)/Aula 134 Express Views/src/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-//FUNCTION DE TRATAMENTO.
-
-exports.paginaInicial = (req, res) => {//Caminho
-    res.render('index');// vai renderizar o index dentro da pasta views.
-};
-
-exports.trataPost = (req, res) => {
-    res.send('Sua nova rota de post');
-};
--- 10 - JAVASCRIPT - NODE, NPM, EXPRESS E MONGODB (NODEJS)/Aula 142 Express Session e Flash Messages/src/controllers/contatoController.js
+++
@@ -1,2 +0,0 @@
-exports.paginaInicial = (req, res, next) => {//Caminho
-    res.send(`Obrigado por entrar em contato.`);};
